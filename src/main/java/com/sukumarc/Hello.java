104e49e0c2651b299d24f1468980a3d702fdedd9
Sukumar Chandrasekaran <mail@sukumarc.com>
2018-12-01 17:02:20 +0530

    Spring intial commit

---
+++ src/main/java/com/sukumarc/Hello.java
@@ -0,0 +1,16 @@
+package com.sukumarc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class Hello {
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String hello() {
+		System.out.println("helllo");
+		return "index";
+	}
+
+}
